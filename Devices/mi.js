649ddddd82ac0df4424de1dab8ea74e08c3be964
Misha Beshkin <misha@kosherdev.com>
2020-10-14 19:18:05 +0300

    fixed mi await for device

--- Devices/mi.js
+++ Devices/mi.js
@@ -6,36 +6,57 @@ app.post('/power', (req, res) => {
     const {key, action, ip} = req.body;
 
     (async () => {
-        const device = miio.device({address: ip, token: key});
-        let status = device.power();
+        const device = await miio.device({address: ip, token: key});
+        let status = await device.power();
         if (action) {
             await device.power(action === 'on');
-            status = device.power();
+            status = await device.power();
         }
         reportWeb(res, device, status)
     })();
 })
 
 app.post('/brightness', (req, res) => {
-    const {key, ip, brightness} = req.body;
+    const {key, action, ip, brightness} = req.body;
 
     (async () => {
-        const device = miio.device({address: ip, token: key});
-        let status = device.power();
+        const device = await miio.device({address: ip, token: key});
+        let status = await device.power();
         let deviceBrightness = 0;
         if (status) {
-            deviceBrightness = device.brightness();
+            deviceBrightness = await device.brightness();
             if (brightness) {
                 await device.setBrightness(brightness);
-                deviceBrightness = device.brightness();
+                deviceBrightness = await device.brightness();
             }
         }
         reportWeb(res, device, status, deviceBrightness)
     })();
+
+    // miio.device({address: ip, token: key})
+    //     .then(device => {
+    //         console.log('Connected to', device)
+    //         device.power().then(isOn => {
+    //             if (brightness) {
+    //                 device.setBrightness(brightness)
+    //                     .then(brightness => {
+    //                         reportWeb(res, device, isOn, brightness)
+    //                     })
+    //                     .catch(err => console.log('Error: ', err));
+    //             } else {
+    //                 device.brightness()
+    //                     .then(brightness => {
+    //                         reportWeb(res, device, isOn, brightness)
+    //                     })
+    //             }
+    //         }).catch(err => console.log('Error: ', err))
+    //     })
+    //     .catch(err => console.log('Error: ', err));
 })
 
 function reportWeb(res, device, deviceStatus = false, brightness = 0) {
     res.send({'success': true, 'device': {'status': deviceStatus, 'brightness': brightness}});
+
     device.destroy();
 }
 
