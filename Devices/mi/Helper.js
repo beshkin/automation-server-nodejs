be54ecb7f0e225ff6f821a85b9d44a5116de1333
Misha Beskin <kosherdev@gmail.com>
2022-07-25 07:21:07 +0300

    fixed mi browser

--- Devices/mi/Helper.js
+++ Devices/mi/Helper.js
@@ -1,19 +1,19 @@
 const miio = require('miio')
 
 async function getDevices() {
-    const browser = miio.browse({
-        cacheTime: 300 // 5 minutes. Default is 1800 seconds (30 minutes)
+    const browser = miio.devices({
+        cacheTime: 1 // 5 minutes. Default is 1800 seconds (30 minutes)
     });
 
     const devices = [];
     browser.on('available', reg => {
         devices.push({id: reg.id.toString(), ip: reg.address})
     });
-
     await new Promise((resolve) => {
         setTimeout(resolve, 2000);
     });
 
+    browser.stop()
     return devices;
 }
 
